674f30a0c6ad2d69a082d5f9b5bb836acbf0f481
magi <alitabaee2002@gmail.com>
2025-03-16 22:15:13 +0330

    inital

---
+++ pages/Experimental.py
@@ -0,0 +1,168 @@
+import streamlit as st
+from sklearn import datasets
+from matplotlib import pyplot as plt
+import seaborn as sb
+import sklearn
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import pickle, os
+from yellowbrick.cluster import KElbowVisualizer
+
+sb.set()
+st.set_page_config(
+    page_title="Experimental",
+    page_icon="🧪"
+)
+
+keys = st.session_state.keys()
+if any(i not in keys for i in
+       ["n_samples", "no_of_saving", "data_preprocessing",
+        "n_features", "centers_std", "seed","xcenters", "lock_seed","xdata","xy","model"]):
+    st.session_state.n_samples = 200
+    st.session_state.n_features = 5
+    st.session_state.xcenters = 2
+    st.session_state.pred_center = 2
+    st.session_state.xdata = 2
+    st.session_state.xy = 2
+    st.session_state.model = 2
+    st.session_state.centers = 2
+    st.session_state.centers_std = 1
+    st.session_state.seed = 42
+    st.session_state.lock_seed = True
+    st.session_state.data_preprocessing = None
+    st.session_state.no_of_saving = 0
+
+st.cache_resource(show_spinner="Loading normalizer")
+
+
+def fixer_preprocessing():
+    try:
+        fixer = getattr(sklearn.preprocessing, st.session_state.data_preprocessing)
+        return fixer
+    except Exception as e:
+        st.error(e)
+
+
+st.cache_data(show_spinner="Creating data ...")
+
+
+def generate_df():
+    n_samples = st.session_state.n_samples
+    n_features = st.session_state.n_features
+    centers = st.session_state.centers
+    centers_std = st.session_state.centers_std
+    data, y, centers = datasets.make_blobs(n_samples=n_samples, n_features=n_features, centers=int(centers),
+                                           shuffle=True, return_centers=True, cluster_std=float(centers_std),
+                                           random_state=st.session_state.seed if st.session_state.lock_seed else None)
+    if st.session_state.data_preprocessing:
+        fixer = fixer_preprocessing()
+        if fixer:
+            f = fixer()
+            data = f.fit_transform(data)
+            centers = f.transform(centers)
+    return data, y, centers
+
+
+st.cache_data()
+
+
+def poof(data, y, centers):
+    fig, ax = plt.subplots()
+    ax.set_title(
+        f"Generated {st.session_state.n_samples}dp features:{st.session_state.n_features}\ncenters:{st.session_state.centers}")
+    ax.scatter(data[:, :1], data[:, 1:2], c=y, cmap="inferno")
+    ax.scatter(centers.T[0], centers.T[1])
+    return fig
+
+
+st.cache_resource(show_spinner="Loading model ...")
+
+
+def model_train():
+    data = st.session_state.xdata
+    y = st.session_state.xy
+    model_input = st.session_state.model
+    if model_input:
+        if model_input == "KNeighborsClassifier":
+            final_model = getattr(sklearn.neighbors, model_input)(n_neighbors=st.session_state.pred_center)
+        elif model_input == "CategoricalNB" or model_input == "MultinomialNB":
+            final_model = getattr(sklearn.naive_bayes, model_input)()
+        elif model_input == "KMeans":
+            final_model = getattr(sklearn.cluster, model_input)(n_clusters=st.session_state.pred_center)
+        elif model_input == "DecisionTreeClassifier":
+            final_model = getattr(sklearn.tree, model_input)()
+        else:
+            st.error("Unknow model or bad parameter!")
+    x_train, x_test, y_train, y_test = train_test_split(data, y, shuffle=True, test_size=0.3)
+    m = final_model
+    m.fit(x_train, y=y_train)
+    preds = m.predict(x_test)
+    if model_input == "KNeighborsClassifier":
+        center = m.classes_
+    elif model_input == "CategoricalNB" or model_input == "MultinomialNB":
+        center = m.classes_
+    elif model_input == "KMeans":
+        center = m.cluster_centers_
+    elif model_input == "DecisionTreeClassifier":
+        center = m.classes_
+    col1, col2 = st.columns([0.75, 0.25])
+    col1.code(sklearn.metrics.classification_report(y_test, preds))
+    if col2.button("KElbow"):
+        try:
+            visualizer = KElbowVisualizer(m, k=(1, 20))
+            visualizer.fit(x_train)
+            fig = plt.gcf()
+            fig.set_size_inches(15, 10)
+            fig.tight_layout()
+            visualizer.show(outpath=None)
+            st.pyplot(fig, clear_figure=True, use_container_width=True)
+            plt.clf()
+        except Exception as e:
+            st.error(e)
+    if col2.button("Save model"):
+        try:
+            pickle.dump(m, open(f"./{model_input}{st.session_state.no_of_saving}.pkl", "wb"))
+            st.session_state.no_of_saving += 1
+            st.balloons()
+        except Exception as e:
+            st.error(e)
+    return x_test, preds, center
+
+
+Tab1, Tab2 = st.tabs(["Generation", "Model"])
+with Tab1:
+    st.header("Data Generation", divider="grey", help="Generate Data for model training")
+    with st.form(key="form_data"):
+        col1, col2, col3 = st.columns(3)
+        col1.number_input(label="n_samples", step=10, min_value=100,
+                          max_value=3000, key="n_samples")
+        col2.number_input(label="n_features", step=1, min_value=2,
+                          max_value=20, key="n_features")
+        col3.number_input(label="centers", step=1, min_value=2,
+                          max_value=10, key="centers")
+        col1.slider(label="cluster_std", step=0.2, min_value=0.5,
+                    max_value=9.0, key="centers_std")
+        col2.selectbox(label="Preprocessing", placeholder="Select a type",
+                       options=[None, "RobustScaler", "StandardScaler", "QuantileTransformer", "PowerTransformer",
+                                "Normalizer"],
+                       key="data_preprocessing")
+        col3.number_input("Seed", step=1, min_value=1, key="seed", disabled=not st.session_state.lock_seed)
+        col3.checkbox("Use Seed", key="lock_seed")
+
+        if st.form_submit_button(type="secondary"):
+            st.session_state.xdata, st.session_state.xy, st.session_state.xcenters = generate_df()
+    st.pyplot(fig=poof(st.session_state.xdata, st.session_state.xy, st.session_state.xcenters))
+
+with Tab2:
+    st.header("Model Training", divider="grey", help="Generate Data for model training")
+    with st.form(key="model_form"):
+        col1, col2, col3 = st.columns(3)
+        col1.selectbox(label="Model",
+                       options=["KNeighborsClassifier",
+                                "KMeans", "CategoricalNB", "MultinomialNB",
+                                "DecisionTreeClassifier"], key="model")
+        col2.number_input("pred_centers", key="pred_center", value=2)
+
+        st.form_submit_button(type="secondary")
+
+    st.pyplot(fig=poof(*model_train()))
---
+++ pages/Regress.py
@@ -0,0 +1,160 @@
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+import sklearn
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPRegressor
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import VotingRegressor
+from sklearn.ensemble import HistGradientBoostingRegressor
+import seaborn as sb
+import numpy as np
+import plotly.graph_objects as go
+from warnings import filterwarnings
+
+filterwarnings('ignore')
+sb.set(style="darkgrid")
+
+if "ticker" not in st.session_state or "preds" not in st.session_state or "days_ahead_prices" not in st.session_state or "data" not in st.session_state:
+    st.session_state.preds = 1
+    st.session_state.ticker = "BTC-USD"
+    st.session_state.data = 1
+
+
+def ticker():
+    data = yf.Ticker(st.session_state.ticker).history(period="max")
+    data = pd.DataFrame(data, columns=["Close", "Open", "Volume"])
+    data["Mid"] = (data["Close"] + data["Open"]) / 2
+    data = data.dropna()
+    data = data[["Volume", "Mid"]]
+    return data
+
+
+# Map for coins with their emojis
+options = {
+    "ADA-USD": "🌱 ADA",
+    "BTC-USD": "₿ BTC",
+    "ETH-USD": "💎 ETH",
+    "XRP-USD": "⚡ XRP",
+    "LTC-USD": "🌕 LTC",
+    "DOGE-USD": "🐕 DOGE",
+    "DOT-USD": "🌐 DOT",
+    "BCH-USD": "🍀 BCH",
+    "SOL-USD": "🌞 SOL",
+    "BNB-USD": "🪙 BNB",
+    "UNI-USD": "🦄 UNI",
+    "LINK-USD": "🔗 LINK",
+    "AAVE-USD": "🏦 AAVE",
+    "XLM-USD": "⭐ XLM",
+    "MATIC-USD": "🔹 MATIC",
+    "MANA-USD": "🕹️ MANA",
+    "SHIB-USD": "🐕‍🦺 SHIB",
+    "CAKE-USD": "🍰 CAKE",
+    "AXS-USD": "🛡️ AXS",
+    "AVAX-USD": "🔥 AVAX",
+    "BUSD-USD": "💵 BUSD",
+    "DAI-USD": "🏅 DAI",
+    "USDT-USD": "💲 USDT",
+    "USDC-USD": "💵 USDC",
+}
+
+st.cache_resource(show_spinner="Loading model ...")
+def modeling():
+    model_input = st.session_state.model
+    data = st.session_state.data
+    data = pd.DataFrame(data)
+    data["x"] = None
+    if model_input:
+        if model_input == "Linear Regression":
+            final_model = LinearRegression()
+        elif model_input == "Support Vector Regression":
+            final_model = SVR()
+        elif model_input == "Decision Tree Regression":
+            final_model = DecisionTreeRegressor()
+        elif model_input == "KNeighbors Regression":
+            final_model = KNeighborsRegressor()
+        elif model_input == "Extra Trees Regression":
+            final_model = ExtraTreesRegressor()
+        elif model_input == "Voting Regression":
+            final_model = VotingRegressor()
+        elif model_input == "HistGradientBoosting Regression":
+            final_model = HistGradientBoostingRegressor()
+        elif model_input == "MLPRegressor":
+            final_model = MLPRegressor()
+        else:
+            st.error("Unknow model or bad parameter!")
+        whole = len(data)
+        batch_size = st.session_state.batch_size
+        x_values = []
+
+        for i in range(batch_size, whole):
+            y = data["Mid"].iloc[i - batch_size:i].to_numpy()
+            x_values.append(y)  # Store as list
+
+        data = data.iloc[batch_size:].copy()  # Remove first few NaN rows
+        data["x"] = x_values  # Assign he collected sequences
+        data = data.dropna()
+        x_train, x_test, y_train, y_test = train_test_split(data["x"].tolist(), data["Mid"], shuffle=False,
+                                                            test_size=0.1)
+        final_model.fit(x_train, y_train)
+        preds = final_model.predict(x_test)
+        days_ahead = pd.date_range(start=data.index[-1], periods=st.session_state.days_ahead, freq="1D")
+        last = x_test[-1].reshape(1, -1)
+        days_ahead_prices = list()
+        for i in range(st.session_state.days_ahead):
+            p = final_model.predict(last)
+            days_ahead_prices.append(p[0])
+            p = np.array(p).reshape(1, 1)  # Ensure p is 2D (1 sample, 1 feature)
+            last = np.append(last[:, 1:], p, axis=1)
+        days_ahead_prices = pd.DataFrame(days_ahead_prices, index=days_ahead)
+        preds = pd.DataFrame({"y": y_test, "preds": preds}, index=y_test.index)
+        st.session_state.preds = preds
+        st.session_state.days_ahead_prices = days_ahead_prices
+
+        return preds, days_ahead_prices
+
+
+tab1, tab2, tab3 = st.tabs(["Data", "Model", "Environment"])
+with tab1:
+    with st.form("my_form"):
+        ticker_choice = st.selectbox("Select Ticker", list(options.values()))
+        ticker_symbol = [key for key, value in options.items() if value == ticker_choice][0]
+        st.session_state.ticker = ticker_symbol  # Set the ticker to session state
+        if st.form_submit_button("Submit"):
+            st.session_state.data = ticker()
+
+with tab2:
+    with st.form("model_form"):
+        col1, col2 = st.columns(2)
+        col1.selectbox("Select Model", ["Linear Regression", "Support Vector Regression", "Decision Tree Regression",
+                                        "KNeighbors Regression",
+                                        "Extra Trees Regression", "Voting Regression",
+                                        "HistGradientBoosting Regression", "MLPRegressor"],
+                       key="model")
+        col2.number_input("Batch size", min_value=1, max_value=31, value=1, step=1, key="batch_size",help="more stable predictions with higher batch size")
+        col2.number_input("Days ahead", min_value=1, max_value=150, value=1, step=1, key="days_ahead",help="The longer periods less accurate predictions")
+        if st.form_submit_button("Submit", on_click=modeling):
+            st.session_state.preds, st.session_state.days_ahead_prices = modeling()
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=st.session_state.data.index,y=st.session_state.data["Mid"], mode="lines",
+                             name=f"{st.session_state.ticker}"))
+    fig.add_trace(
+        go.Scatter(x=st.session_state.preds.index,y=st.session_state.preds.preds, mode="lines", name="Validation"))
+
+    fig.add_trace(
+        go.Scatter(x=st.session_state.days_ahead_prices.index,y=st.session_state.days_ahead_prices[0], mode="lines",
+                   name="Future Predictions", line=dict(color='cyan', width=3)))
+    fig.update_layout(
+        title=f"Data for {st.session_state.get('ticker', 'Unknown')}",
+        xaxis_title="Time",
+        yaxis_title="Price",
+        template="plotly_white",
+        showlegend=True,
+        hovermode="x unified",
+        legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1))
+    st.plotly_chart(fig, use_container_width=True)
+    st.table(st.session_state.days_ahead_prices)
