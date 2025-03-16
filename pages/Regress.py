01307fb3618fb224a7c24997c5bd2745968dfe1e
magi <alitabaee2002@gmail.com>
2025-03-16 22:21:14 +0330

    inital

--- pages/Regress.py
+++ pages/Regress.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import yfinance as yf
 import pandas as pd
+import sklearn
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
